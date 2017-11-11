c0fa1b09deda797284a6467cd639cf3d9fe4f985
Dylan <Dylan@134.87.155.159>
2017-11-10 18:49:17 -0800

    Pushing base Vehicle class Added type, license, status

---
+++ HelloSpring/src/WEBAPI/Vehicle.java
@@ -0,0 +1,35 @@
+package WEBAPI;
+
+public class Vehicle{
+	private String type;
+	private String license;
+	private int status;
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getType() {
+		return type;
+	}
+	
+	public void setLicense(String license) {
+		this.license = license;
+	}
+	public String getLicense() {
+		return license;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getStatus() {
+		return status;
+	}
+	
+	
+}
+
+
+
+
+
