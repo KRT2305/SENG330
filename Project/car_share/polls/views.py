5a01ba53bd80fc0b09dcfea3641cb8361eabd788
Dvorachek <Dylan.Dvorachek@gmail.com>
2017-11-22 19:57:28 -0800

    changed index page to display customers in order by name

--- Project/car_share/polls/views.py
+++ Project/car_share/polls/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Customer
+
 def index(request):
-    return HttpResponse("Polls index.")
+    customer_list = Customer.objects.order_by('name')
+    output = ', '.join([str(customer) for customer in customer_list])
+    return HttpResponse(output)
 
 def detail(request, customer_id):
     return HttpResponse("You're viewing customer %s" % customer_id)
