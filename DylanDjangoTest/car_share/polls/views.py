8d23e8a9f11dc402675d63eeb06f3f7e92a328dc
Dvorachek <Dylan.Dvorachek@gmail.com>
2017-11-22 18:59:12 -0800

    added detail and booking views for customer

--- DylanDjangoTest/car_share/polls/views.py
+++ DylanDjangoTest/car_share/polls/views.py
@@ -2,3 +2,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Polls index.")
+
+def detail(request, customer_id):
+    return HttpResponse("You're viewing customer {}".format(customer_id)
+
+def bookings(request, customer_id):
+    response = "You're looking at the bookings of customer %s."
+    return HttpResponse(response % customer_id)
+
+
+
+
+
