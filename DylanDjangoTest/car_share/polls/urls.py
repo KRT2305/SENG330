4c377891da9801379ba49a3b3e79f11394b51f13
Dvorachek <Dylan.Dvorachek@gmail.com>
2017-11-22 19:07:28 -0800

    Added customer detail/bookings to urls

--- DylanDjangoTest/car_share/polls/urls.py
+++ DylanDjangoTest/car_share/polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<customer_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<customer_id>[0-9]+)/bookings/$', views.bookings, name='bookings'),
 ]
 	
