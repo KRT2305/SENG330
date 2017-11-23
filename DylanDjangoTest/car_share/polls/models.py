18cc5c3c9934472cbe9154cca8c557d111bb6fe7
Dvorachek <Dylan.Dvorachek@gmail.com>
2017-11-22 17:55:53 -0800

    added base customer class

--- DylanDjangoTest/car_share/polls/models.py
+++ DylanDjangoTest/car_share/polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+	name = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	phone_number = models.CharField(max_length=200)
+	email = models.CharField(max_length=200)
+	licence = models.CharField(max_length=200)
