be30dc4aa4ef54c105758fb528a72c9c7d15c36a
Dvorachek <Dylan.Dvorachek@gmail.com>
2017-11-24 18:34:53 -0800

    added models

--- Project/car_share/auto/models.py
+++ Project/car_share/auto/models.py
@@ -9,33 +9,66 @@ class Customer(models.Model):
 	licence = models.CharField(max_length=200)
 
 	#customer constructor
-	def __init__(self,name,address,phone_number,email,licence):
+	def __init__(self, name, address, phone_number, email,licence):
 		self.name = name
 		self.address = address
 		self.phone_number = phone_number
 		self.email = email
 		self.licence = licence
 
+	# just playing around with this..
+
+	'''
+	def __iter__(self):
+		for attr, value in self.__dict__.iteritems():
+			yield attr, value
+	'''
+	# to string
 	def __str__(self):
-		return self.name
+		output = ''.join(['{}:: {}\n'.format(attr, value) for attr, value in self.__dict__.items()])
+		return output
 
-
-class Booking(models.Model):
-	customer = Customer()
-	vehicle = Vehicle()
-	depot = Depot()
-	booking_time = datetime.datetime
-
-	#def create_booking(self, customer, vehicle, depot, booking_time):
 
 class Depot(models.Model):
 	address = models.CharField(max_length=200)
-	#inventory
-	#schedule
+	
+	'''
+	Might want to reconsider making depot aware of schedule, since we could
+	add a way for schedule to be viewed from admin page. Then we add a way
+	to search for schedule by depot. ie Schedule knows about depot, but depot
+	doesn't know about schedule.
+	
+	Add a queury so grabbing all vehicles associated with it.
+	'''
+	
 
 class Vehicle(models.Model):
-	depot = Depot()
-	is_available = model.BooleanField(initial=True)
-	type = models.CharField(max_length=200)
-	license_plate = models.CharField(max_length=20)
+	depot = models.ForeignKey(Depot, on_delete=models.PROTECT)
+	available = models.BooleanField(True)
+	v_type = models.CharField(max_length=200)
+	license = models.CharField(max_length=200)
 	# schedule
+
+	def __init__(self, depot, available, v_type, license):
+		self.depot = depot
+		self.available = available
+		self.v_type = v_type
+		self.license = license
+
+	def set_status(self, status):
+		self.is_available = status
+
+	def get_status(self):
+		return self.is_available
+
+class Booking(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+	vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+	depot = models.ForeignKey(Depot, on_delete=models.CASCADE)
+	booking_time = models.DateTimeField('date booked')
+	
+	def __init__(self, customer, vehicle, depot, date):
+		self.customer = customer
+		self.vehicle = vehicle
+		self.depot = depot
+		self.booking_time = date
