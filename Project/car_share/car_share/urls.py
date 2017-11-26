47974a4b457d66d033660f189e8ecfac285a66e9
Rowena Zhu <rowzhu@gmail.com>
2017-11-25 19:44:54 -0800

    Added customer login page at /accounts/login

--- Project/car_share/car_share/urls.py
+++ Project/car_share/car_share/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^auto/', include('auto.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
 ]
