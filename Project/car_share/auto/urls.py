9a7ee008d06dc52b4b941457e7a6885a47be1f51
samuel tsokwa <samueltsokwa@yahoo.com>
2017-11-29 13:36:50 -0800

    Update urls.py

--- Project/car_share/auto/urls.py
+++ Project/car_share/auto/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     url(r'^(?P<customer_id>[0-9]+)/profile/$', views.profile, name='profile'),
     url(r'^(?P<customer_id>[0-9]+)/create_booking/$', views.create_booking, name='create_booking'),
     url(r'^(?P<customer_id>[0-9]+)/my_bookings/$', views.my_bookings, name='my_bookings'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.activate, name='activate'),
 ]
 	
