4240e2dcd81e029cb80443516a77239443bd7c07
Dvorachek <Dylan.Dvorachek@gmail.com>
2017-11-29 21:42:42 -0800

    added basic booking test

--- Project/car_share/auto/tests.py
+++ Project/car_share/auto/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Customer, Vehicle, Depot, Booking
 
-# Create your tests here.
+import datetime
+
+def test():
+	c = Customer.objects.get(id=1)
+	d = Depot.objects.get(id=1)
+	v = Vehicle.objects.get(id=1)
+
+	now = datetime.datetime.now()
+
+	tom = now + datetime.timedelta(days=2)
+
+	b = Booking.objects.create_booking(c, v, d, now, tom)
+
+	print(b)
+
+
+
+
