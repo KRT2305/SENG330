3bccd508302fdf2f4c123e29dd65fe91840a60df
Rowena Zhu <rowzhu@gmail.com>
2017-11-30 17:10:36 -0800

    commented out time object

--- Project/car_share/auto/managers.py
+++ Project/car_share/auto/managers.py
@@ -43,16 +43,18 @@ class VehicleManager(models.Manager):
 		else:
 			return self.get_queryset().vehicles(depot, v_type)
 
-class TimeManager(models.Manager):
+'''class TimeManager(models.Manager):
 	def time_is_valid(self,time):
 		if start_time > end_time:
 			return 0
 		td = end_time - start_time
-		return td
+		return td '''
 
 class BookingManager(models.Manager):
-	def create_booking(self, customer, vehicle, depot, time):
-		is_valid(time)
+	def create_booking(self, customer, vehicle, depot, start_time, end_time):
+		if start_time > end_time:
+			return 0
+		td = end_time - start_time
 		days, seconds = td.days, td.seconds
 		hours = days * 24 + seconds // 3600
 		if hours > int(customer.email):
--- Project/car_share/auto/models.py
+++ Project/car_share/auto/models.py
@@ -55,24 +55,25 @@ class Vehicle(models.Model):
 		return "{}\n{}\n{}".format(self.depot, self.license, self.v_type)
 		# return ''.join(['{}:: {}\n'.format(attr, value) for attr, value in self.__dict__.items()])
 
-class Time(models.Model):
+'''class Time(models.Model):
 	start_time = models.DateTimeField(null=True)
 	end_time = models.DateTimeField(null=True)
 
 	objects = TimeManager()
 
 	def __str__(self):
-		return "{}\n{}".format(self.start_time, self.end_time)
+		return "{}\n{}".format(self.start_time, self.end_time) '''
 
 
 class Booking(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
 	vehicle = models.ForeignKey(Vehicle,on_delete=models.CASCADE, related_name='vehicle')
 	depot = models.ForeignKey(Depot, on_delete=models.CASCADE, related_name='depot')
-	time = models.ForeignKey(Time, on_delete=models.CASCADE, related_name='time')
+
+	start_time = models.DateTimeField(null=True)
+	end_time = models.DateTimeField(null=True)
     
 	objects = BookingManager()
 
 	def __str__(self):
-		return "{}\n{}\n{}\n{}".format(self.customer.username, self.vehicle.license, self.depot.name, self.time)
-
+		return "{}\n{}\n{}\n{}\n{}".format(self.customer.username, self.vehicle.license, self.depot.name, self.start_time, self.end_time)
